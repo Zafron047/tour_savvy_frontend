7e2bd0c612d294878f95a0eacb8dbd58ac626a4e
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-29 20:54:49 +0600

    Modify index.js and app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-// import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Packages from './components/Packages';
+import Nav from './components/Nav';
 
-function App() {
-  return (
-    <h1 className="display-4">Hello World</h1>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit
-    //       {' '}
-    //       <code>src/App.js</code>
-    //       {' '}
-    //       and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
-}
+const App = () => (
+  <>
+    <Nav />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Packages />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+);
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Nav = () => (
+  <h1>Side Navigation bar</h1>
+);
+
+export default Nav;
