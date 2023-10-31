8fcdab9302b23ea23347e065585467a57fd649af
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-31 21:46:01 +0600

    Add reservation slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import packagesSlice from './packages/packagesSlice';
+import reservationsReducer from "./reservations/reservationSlice";
 
 const loadStateFromLocalStorage = () => {
   try {
@@ -16,6 +17,7 @@ const loadStateFromLocalStorage = () => {
 const store = configureStore({
   reducer: {
     packages: packagesSlice,
+    reservations: reservationsReducer,
   },
   preloadedState: loadStateFromLocalStorage(),
 });
