602e3fc573c8739f8e507d2375fd8d16f87aff20
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-11-01 02:08:27 +0600

    Fix routes for add/remove functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { useDispatch } from 'react-redux';
 import Packages from './components/Packages';
 import PackageDetails from './components/Package_Details';
 import PackageForm from './components/PackageForm';
 import SideNav from './components/Nav';
 import DeletePackages from './components/DeletePackages';
+import { getReservations } from './redux/reservations/reservationSlice';
 import Reservations from './components/Reservations';
+import AddReservation from './components/AddReservation';
+import RemoveReservation from './components/RemoveReservation';
 
-const App = () => (
-  <>
-    <SideNav />
-    <Routes>
-      <Route path="/" element={<Packages />} />
-      <Route path="/details/:id" element={<PackageDetails />} />
-      <Route path="/add_package" element={<PackageForm />} />
-      <Route path="/delete-packages" element={<DeletePackages />} />
-      <Route path="/reservations" element={<Reservations />} />
-    </Routes>
-  </>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getReservations());
+  }, []);
+
+  return (
+    <>
+      <SideNav />
+      <Routes>
+        <Route path="/" element={<Packages />} />
+        <Route path="/details/:id" element={<PackageDetails />} />
+        <Route path="/add_package" element={<PackageForm />} />
+        <Route path="/delete-packages" element={<DeletePackages />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/add_reservations" element={<AddReservation />} />
+        <Route path="/remove_reservations" element={<RemoveReservation />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
