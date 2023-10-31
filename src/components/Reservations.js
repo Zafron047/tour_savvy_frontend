327ac509ca6d644923f0fba3b1deea120340153a
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-11-01 02:07:52 +0600

    Add add/remove reservation functionalities

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,16 +1,8 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getReservations } from '../redux/reservations/reservationSlice';
 
 function Reservations() {
   const { reservations, isLoading } = useSelector((store) => store.reservations);
-  // console.log(reservations);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getReservations(1));
-  }, []);
-
   if (isLoading) {
     return (
       <>
--- src/redux/reservations/reservationSlice.js
+++ src/redux/reservations/reservationSlice.js
@@ -5,17 +5,54 @@ const initialState = {
   reservations: [],
   isLoading: true,
 };
+
 export const getReservations = createAsyncThunk(
   'reservations/getReservations',
-  async (thunkAPI, id) => {
+  async (thunkAPI) => {
     try {
-      const res = await axios(`http://127.0.0.1:3000/packages/${id}/reservations`);
+      const res = await axios('http://127.0.0.1:3000/reservations');
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue('something went wrong');
     }
   },
 );
+
+export const addReservation = createAsyncThunk(
+  'reservations/addReservations',
+  async (reservation, thunkAPI) => {
+    const newReservation = {
+      city_name: reservation.cityName,
+      reservation_date: reservation.reservationDate,
+      package_name: reservation.packageName,
+      package_type: reservation.packageType,
+    };
+    try {
+      const res = await axios.post(
+        'http://127.0.0.1:3000/reservations/', newReservation,
+      );
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  },
+);
+
+export const removeReservation = createAsyncThunk(
+  'reservations/removeReservations',
+  async (id, thunkAPI) => {
+    console.log(id);
+    try {
+      const res = await axios.delete(
+        `http://127.0.0.1:3000/reservations/${id}`,
+      );
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  },
+);
+
 const reservationsSlice = createSlice({
   name: 'reservations',
   initialState,
@@ -29,6 +66,15 @@ const reservationsSlice = createSlice({
     },
     [getReservations.rejected]: (state) => {
       state.isLoading = false;
+    },
+    [addReservation.fulfilled]: (state, action) => {
+      state.reservations.push(action.payload);
+    },
+    [removeReservation.fulfilled]: (state, action) => {
+      console.log(action.payload);
+      state.reservations = state.reservations.filter(
+        (reservation) => reservation.id !== action.payload.id,
+      );
     },
   },
 });
