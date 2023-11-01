f6aa5ed0924159d772d69494fddba2bbb558f92b
MARVIN <o.silverygold@gmail.com>
2023-11-01 13:16:32 +0300

    Add current user to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import packagesSlice from './packages/packagesSlice';
 import reservationsReducer from './reservations/reservationSlice';
+import userSlice from './authentication/userSlice';
 
 const loadStateFromLocalStorage = () => {
   try {
@@ -14,12 +15,28 @@ const loadStateFromLocalStorage = () => {
   }
 };
 
+const loadUserFromLocalStorage = () => {
+  try {
+    const serializedUser = localStorage.getItem('user');
+    if (serializedUser === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedUser);
+  } catch (err) {
+    return undefined;
+  }
+};
+
 const store = configureStore({
   reducer: {
     packages: packagesSlice,
     reservations: reservationsReducer,
+    current_user: userSlice,
   },
-  preloadedState: loadStateFromLocalStorage(),
+  preloadedState: {
+    current_user: loadUserFromLocalStorage(),
+    ...loadStateFromLocalStorage(),
+  },
   reservations: reservationsReducer,
 });
 
