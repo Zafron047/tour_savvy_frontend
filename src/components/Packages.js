23ad69e8ca11df54733ff98a0ca86abaa4983a3f
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-11-02 20:39:44 +0600

    Add search field in packages page

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Package from './Package';
 import css from '../stylesheets/packages.module.css';
-
 const Packages = () => {
   const packages = useSelector((state) => state.packages.allPackages);
   const loading = useSelector((state) => state.packages.loading);
-
+  const [searchQuery, setSearchQuery] = useState('');
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
   if (loading) {
     return (
       <>
@@ -16,18 +18,23 @@ const Packages = () => {
       </>
     );
   }
-
+  const filteredPackages = packages.filter((p) =>
+    p.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <div className={css.container}>
       <h1 className={css.headline}>TOUR PACKAGES</h1>
       <p className={css.desc}>please select a tour package</p>
-
-      { packages.map((p) => (
+      <input
+        type="text"
+        placeholder="Search by package name"
+        value={searchQuery}
+        onChange={handleSearchChange}
+      />
+      { filteredPackages.map((p) => (
         <Package key={p.id} p={p} />
       ))}
-
     </div>
   );
 };
-
 export default Packages;
