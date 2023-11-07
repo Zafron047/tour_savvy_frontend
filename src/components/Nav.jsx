fc548a5f8a5341647e31ffe9b567152fc9fd8db9
MARVIN <o.silverygold@gmail.com>
2023-11-07 15:50:04 +0300

    Add navbar styling and logic

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,41 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import {
+  FaFacebookF, FaGooglePlusG, FaVimeoV, FaPinterestP,
+} from 'react-icons/fa';
+import { BsTwitter } from 'react-icons/bs';
+import { AiOutlineCopyrightCircle } from 'react-icons/ai';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import logoImage from './Assets/logotour.png';
 import '../stylesheets/nav.css';
 
 const SideNav = () => {
   const user = JSON.parse(localStorage.getItem('user')) || {};
+  const [isNavVisible, setNavVisible] = useState(false);
+  const toggleSidebar = () => {
+    setNavVisible(!isNavVisible);
+  };
+
+  const renderLoginOrLogoutLink = () => {
+    if (user && user.user) {
+      return (
+        <li className="sidenav-item">
+          <NavLink to="/logout" className="sidenav-link" onClick={toggleSidebar}>Logout</NavLink>
+        </li>
+      );
+    } else {
+      return (
+        <li className="sidenav-item">
+          <NavLink to="/" className="sidenav-link" onClick={toggleSidebar}>Login</NavLink>
+        </li>
+      );
+    }
+  };
+
   return (
-    <div className="nav">
-      <ul>
-        <li>
-          <NavLink to="/packages">Packages</NavLink>
-        </li>
-        <li>
-          <NavLink to="/reservations">Reservations</NavLink>
-        </li>
-        {user && user.user && user.user.admin === true && (
+    <div className="nav-container d-flex flex-column align-items-center">
+      <nav className="burger">
+        <button type="button" className="toggle-button" onClick={toggleSidebar}>
+          <GiHamburgerMenu />
+        </button>
+      </nav>
+      <nav className={`nav ${isNavVisible ? 'show' : 'hidden'}`}>
+        <div className="logo-img">
+          <img className="logo" src={logoImage} alt="logo" />
+        </div>
+        <ul className="sidenav-list d-flex flex-column justify-content-center align-items-center">
+          <li className="sidenav-item">
+            <NavLink to="/packages" className="sidenav-link" onClick={toggleSidebar}>
+              Packages
+            </NavLink>
+          </li>
+          <li className="sidenav-item">
+            <NavLink to="/reservations" className="sidenav-link" onClick={toggleSidebar}>
+              Reservations
+            </NavLink>
+          </li>
+          {user && user.user && user.user.admin === true && (
           <>
-            <li>
-              <NavLink to="/add_package">Add Package</NavLink>
+            <li className="sidenav-item">
+              <NavLink to="/add_package" className="sidenav-link" onClick={toggleSidebar}>
+                Package +
+              </NavLink>
             </li>
-            <li>
-              <NavLink to="/delete-packages">Delete Package</NavLink>
+            <li className="sidenav-item">
+              <NavLink to="/delete-packages" className="sidenav-link" onClick={toggleSidebar}>
+                Package -
+              </NavLink>
             </li>
           </>
-        )}
-        <li>
-          <NavLink to="/add_reservations">Add Reservation</NavLink>
-        </li>
-        <li>
-          <NavLink to="/remove_reservations">Remove Reservations</NavLink>
-        </li>
-        <li>
-          <NavLink to="/registration">Register</NavLink>
-        </li>
-        <li>
-          <NavLink to="/logout">Logout</NavLink>
-        </li>
-      </ul>
+          )}
+          <li className="sidenav-item">
+            <NavLink to="/add_reservations" className="sidenav-link" onClick={toggleSidebar}>
+              Reservation +
+            </NavLink>
+          </li>
+          <li className="sidenav-item">
+            <NavLink to="/remove_reservations" className="sidenav-link" onClick={toggleSidebar}>
+              Reservations -
+            </NavLink>
+          </li>
+          <li className="sidenav-item">
+            <NavLink to="/registration" className="sidenav-link" onClick={toggleSidebar}>
+              Register
+            </NavLink>
+          </li>
+            {renderLoginOrLogoutLink()}
+        </ul>
+        <div className="sidenav-footer">
+          <ul className="sidebar-socials">
+            <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <BsTwitter />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <FaFacebookF />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <FaGooglePlusG />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <FaVimeoV />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <FaPinterestP />
+              </NavLink>
+            </li>
+          </ul>
+          <p className="copyright">
+            <span><AiOutlineCopyrightCircle /></span>
+            2023 TourSavvy
+          </p>
+        </div>
+      </nav>
     </div>
   );
 };
